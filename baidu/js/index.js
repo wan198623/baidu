3374f08365f763403e68f5a9cbf9b85f0fd31255
wan198623 <1592866194@qq.com>
2016-11-30 01:50:10 +0800

    dd

---
+++ baidu/js/index.js
@@ -0,0 +1,37 @@
+$(function(){
+    var num=0;
+    var clientH=$(window).height();
+    var flag=true;
+    touch.on("body","swipeup","#fullpage",function(e){
+        if(!flag){
+            return;
+        }
+        num++;
+        if(num==$("section").length){
+            num=$("section").length-1;
+            return;
+        }
+        flag=false;
+        $("#fullpage").css({
+            marginTop:-num*clientH
+        })
+    })
+    touch.on("body","swipedown","#fullpage",function(e){
+        if(!flag){
+            return;
+        }
+        num--;
+        if(num==-1){
+            num=0;
+            return;
+
+        }
+        flag=false;
+        $("#fullpage").css({
+            marginTop:-num*clientH
+        })
+    })
+    $("#fullpage")[0].addEventListener("webkitTransitionEnd",function(){
+        flag=true;
+    })
+})
